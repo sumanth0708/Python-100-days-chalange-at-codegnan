561d42b717a6b56fb5881351b02ab9aa4ed30677
Sumanth Reddy Vintha <sumanthreddyvintha24@gmail.com>
2026-01-07 17:04:11 +0530

    Add files via upload
    Datatypes variables and IO functions

---
+++ Day-1.py
@@ -0,0 +1,3 @@
+print("my self sumanth")
+print("day 1 python full stack")
+print("I am performing some operations")
---
+++ Day-2 Datatypes and veriables.py
@@ -0,0 +1,17 @@
+#Datatype, variables and IQ functions
+name = "Sumanth" #String type
+age = 21 #Integer type
+weight = 80.52 #Float type
+list1 = [1,3,4,6,'hello',6,(20,30)] #List of items
+tuple1 = (30,40,"welcome") #tuple of items
+cards = {'A',2,4,6,4,'c','t','M'} #Set datatype
+student_marks = {'maths' :90, "Telugu" :89, "English" :92} #Dict datatype
+
+
+print("My name is:" ,name)
+print("My age is:" ,age)
+print("And my weight is:" ,weight)
+print("List of items:" ,list1)
+print("list of tuples:" ,tuple1)
+print("The cards values:" ,cards)
+print("Student all subject marks:" ,student_marks)
