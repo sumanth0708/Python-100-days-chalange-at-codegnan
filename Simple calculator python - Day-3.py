e68eb92609baa7ef8b1f957eed1eecec4ef2dfce
Sumanth Reddy Vintha <sumanthreddyvintha24@gmail.com>
2026-01-08 17:02:29 +0530

    Add files via upload
    I'am aiming to crack the IT job in 100 days.
    and I'am present a python full stack trainee in #codegnan.

---
+++ Simple calculator python - Day-3.py
@@ -0,0 +1,12 @@
+#simple calculator operations
+#variable and operators
+a=10
+b=20
+addition=a+b
+print ("sum of two numbers is:", addition)
+print ("subtraction of two numbers is:", a-b)
+print ("Multiplication of two numbers is:"
+print ("Division of two numbers is: ",b/a)
+print ("Floor division of two numbers:", b//a)
+print ("Module division of two numbers:" ,bsa)
+print ("power of two numbers:", b**a)
